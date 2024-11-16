9588564aa60db421d5b56983115f9370450c970f
devilanirudh <anirudhdevs91@gmail.com>
2024-11-17 00:46:29 +0530

    first commit

---
+++ todoApp/main.py
@@ -0,0 +1,14 @@
+
+from routers import auth,todos
+
+from fastapi import FastAPI
+
+import models
+from database import engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
